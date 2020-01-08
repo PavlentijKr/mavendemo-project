c7376fa20b8108b081193cb2ace45afb1d3d97e0
Pavel Krylov <pkrylov@gmail.com>
2020-01-08 11:29:41 +0300

    Commiting changes

--- mavendemo/src/main/java/com/learnvaven/mavendemo/App.java
+++ mavendemo/src/main/java/com/learnvaven/mavendemo/App.java
@@ -9,15 +9,15 @@ import org.apache.log4j.Logger;
 
 public class App 
 {
-    final static Logger logger = Logger.getLogger(App.class);
+    final static Logger LOGGER = Logger.getLogger(App.class);
 
     public static void main( String[] args )
     {
         Calculator calc = new Calculator();
 
-        logger.debug(calc.summ(2,5));
-        logger.debug(calc.subtract(2,5));
-        logger.debug(calc.multiply(2,5));
-        logger.debug(calc.divide(2,5));
+        LOGGER.debug(calc.summ(2,5));
+        LOGGER.debug(calc.subtract(2,5));
+        LOGGER.debug(calc.multiply(2,5));
+        LOGGER.debug(calc.divide(2,5));
     }
 }
